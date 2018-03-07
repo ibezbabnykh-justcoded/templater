5bcde377fc848baf5cb4630312a195734e6a5c59
Igor Bezbabnykh <ibezbabnykh@justcoded.co>
2018-03-07 17:01:24 +0200

    stage 3

--- templater.js
+++ templater.js
@@ -1,22 +1,28 @@
 var Templater = {
 	tags: {},
+	render: function(template, element) {
+		return template.replace(/{{(.*?)}}/g, function(match, template) {
+			if(template === 'html') {
+				return element.innerHTML;
+			} else {
+				return element.getAttribute(template);
+			}
+		});
+	},
 	addTag: function(tagName, template) {
 		this.tags[tagName] = template;
 	},
 	run: function() {
-		for(var customTag in this.tags) {
-			let elements = document.getElementsByTagName(customTag);
+		for(let customTag in this.tags) {
+			let elements = Array.from(document.getElementsByTagName(customTag));
 			let length = elements.length;
 			if (!length) {
-				continue;
+				return;
 			}
 
-			for(let i = 0; i < length; i++) {
-				let element = elements[0];
-				element.outerHTML = this.tags[customTag];
-
-			}
+			elements.forEach((element) => {
+				element.outerHTML = this.render(this.tags[customTag], element);
+			});
 		};
 	}
-
 }
